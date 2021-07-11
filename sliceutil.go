8a88f1c86b35ec9392e27a20494362fcbf8c4379
samy <samy1090900@outlook.com>
2021-07-11 23:21:30 +0800

    update

---
+++ sliceutil.go
@@ -0,0 +1,42 @@
+package sliceutil
+
+//
+func InArray(needle interface{}, hystack interface{}) bool {
+	switch key := needle.(type) {
+	case string:
+		for _, item := range hystack.([]string) {
+			if key == item {
+				return true
+			}
+		}
+	case int:
+		for _, item := range hystack.([]int) {
+			if key == item {
+				return true
+			}
+		}
+	case int64:
+		for _, item := range hystack.([]int64) {
+			if key == item {
+				return true
+			}
+		}
+	default:
+		return false
+	}
+	return false
+}
+
+//Removes duplicate values from an array
+func ArrayUnique(arr []string) []string {
+	size := len(arr)
+	result := make([]string, 0, size)
+	temp := map[string]struct{}{}
+	for i := 0; i < size; i++ {
+		if _, ok := temp[arr[i]]; ok != true {
+			temp[arr[i]] = struct{}{}
+			result = append(result, arr[i])
+		}
+	}
+	return result
+}
